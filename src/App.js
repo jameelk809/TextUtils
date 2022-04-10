27247504fd1a9e1397afe7d7f3b36975e7e22aec
Kamran <jameelk809@gmail.com>
2022-04-11 01:20:17 +0530

    color palettes added
    toggle switch removed,
    
    this will be removed in next commit.

--- src/App.js
+++ src/App.js
@@ -23,7 +23,18 @@ function App() {
     }, 2000);
   }
 
-  const toggleMode = () => {
+  const removeBodyClasses = ()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
+
+  }
+
+  const toggleMode = (cls) => {
+    removeBodyClasses();
+    document.body.classList.add('bg-'+cls)
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = 'black';
