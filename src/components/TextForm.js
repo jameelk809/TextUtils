f382d7e1b0d917b49f47043523353e28b5ddf7f7
Kamran <jameelk809@gmail.com>
2022-04-11 22:44:24 +0530

    removed selection & deselection of text in Navigator API

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -30,10 +30,7 @@ export default function TextForm(props) {
     }
 
     const handleCopy = () => {
-        var text = document.getElementById('myBox');
-        text.select();
         navigator.clipboard.writeText(text.value);
-        document.getSelection().removeAllRanges();
         props.showAlert("Copied to clipboard", "success");
     }
 
